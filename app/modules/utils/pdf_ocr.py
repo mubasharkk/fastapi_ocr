0d0ea973ae595b8b0a139d820efea72ea8ebbc4a
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2024-05-18 19:10:55 +0200

    Multipage PDF from multipage image added
    Removed DB handling

--- app/modules/utils/pdf_ocr.py
+++ app/modules/utils/pdf_ocr.py
@@ -2,7 +2,7 @@ import uuid
 import ocrmypdf
 
 
-def convert_pdfa(input_file: str, lang='eng') -> str:
+def convert_pdfa(input_file: str, lang: str = 'eng', force_ocr: bool = True, skip_text: bool = True) -> str:
     output_file = f'/tmp/{uuid.uuid4()}.pdf'
     ocrmypdf.ocr(
         input_file,
@@ -12,17 +12,16 @@ def convert_pdfa(input_file: str, lang='eng') -> str:
         rotate_pages=True,
         jobs=6,
         # redo_ocr=True,
-        force_ocr=True,
+        force_ocr=force_ocr,
         clean=True,
         invalidate_digital_signatures=True,
         keep_temporary_files=False,
         optimize=1,
         # skip_big=5
-        # skip_text=True
+        skip_text=skip_text
     )
     return output_file
 
 
 def perform_ocr_on_storage_file(key: str, bucket: str):
-    
     return key
